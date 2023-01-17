452819b974d5a42586f77749d4216fb010bf1b8a
jobst <j.obst@teb-akademia.pl>
2023-01-17 19:10:42 +0100

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,7 +62,7 @@ module.exports = {
           primary: "#6366F1",
           secondary: "#8587f3",
           accent: "#e057ff",
-          neutral: "rgba(255,255,255,0.8)",
+          neutral: "rgb(250, 250, 250)",
           "base-100": "#ffffff",
         },
       },
