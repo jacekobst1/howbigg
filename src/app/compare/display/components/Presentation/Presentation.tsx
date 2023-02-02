0ebab95fa5b256242f05dcc349015e8278dd7083
jobst <j.obst@teb-akademia.pl>
2023-02-02 20:38:50 +0100

    Create hook useWindowDimensions. Add dynamic displays border width

--- src/app/compare/display/components/Presentation/Presentation.tsx
+++ src/app/compare/display/components/Presentation/Presentation.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import Display from "@/app/compare/display/types/Display";
+import { getWindowDimensions } from "@/hooks/useWindowDimensions";
 
 interface SetupProps {
   displays: Display[];
 }
 
 export default function Presentation({ displays }: SetupProps) {
+  const windowDimensions = getWindowDimensions();
+  const borderSize = windowDimensions.width >= 768 ? 3 : 2;
+
   return (
     <div className="w-11/12 mx-auto">
       <div className="w-full relative" style={{ aspectRatio: "1/1" }}>
@@ -23,7 +27,7 @@ export default function Presentation({ displays }: SetupProps) {
               zIndex: display.zIndex,
               border:
                 display.height.percentage * display.width.percentage > 0
-                  ? "3px solid black"
+                  ? `${borderSize}px solid black`
                   : "none",
             }}
           />
