b699904eaa7528643d552a750426c14536647349
jobst <j.obst@teb-akademia.pl>
2023-02-03 18:58:45 +0100

    Remove redundant meta tags

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,31 +5,25 @@ export default function Head() {
     title: "How big is your display",
     siteName: "Howbigg",
     description: "Compare size of displays",
-    // url: "https://tsnext-tw.thcl.dev",
+    url: "https://howbigg.com",
     type: "website",
     robots: "follow, index",
   };
 
   return (
     <>
-      <title>How big is your display</title>
-      <meta charSet="utf-8" />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Compare size of displays" />
-      <meta name="og:title" content="How big is your display" />
-      <meta name="og:description" content="Compare size of displays" />
-      <meta name="og:site_name" content="Howbigg" />
-      <link rel="icon" href="/favicon/favicon.ico" />
-
       <title>{meta.title}</title>
       <meta name="robots" content={meta.robots} />
-      <meta content={meta.description} name="description" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content={meta.description} />
+      <link rel="canonical" href={meta.url} />
 
       {/* Open Graph */}
-      <meta property="og:type" content={meta.type} />
+      <meta property="og:title" content={meta.title} />
       <meta property="og:site_name" content={meta.siteName} />
       <meta property="og:description" content={meta.description} />
-      <meta property="og:title" content={meta.title} />
+      <meta property="og:url" content={meta.url} />
+      <meta property="og:type" content={meta.type} />
 
       {/* Twitter */}
       <meta name="twitter:title" content={meta.title} />
@@ -68,7 +62,7 @@ const favicons: Array<React.ComponentPropsWithoutRef<"link">> = [
   {
     rel: "mask-icon",
     href: "/favicon/safari-pinned-tab.svg",
-    color: "#00e887",
+    color: "#6366f1",
   },
   { rel: "shortcut icon", href: "/favicon/favicon.ico" },
 ];
