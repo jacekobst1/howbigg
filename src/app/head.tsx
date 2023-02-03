c510360bccc7cd70a8f36c3f066f0eecc05cb871
jobst <j.obst@teb-akademia.pl>
2023-02-03 18:23:51 +0100

    Upgrade main Head component and favicons

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,74 @@
+import React from "react";
+
 export default function Head() {
+  const meta = {
+    title: "How big is your display",
+    siteName: "Howbigg",
+    description: "Compare size of displays",
+    // url: "https://tsnext-tw.thcl.dev",
+    type: "website",
+    robots: "follow, index",
+  };
+
   return (
     <>
-      <title>How big is your...</title>
+      <title>How big is your display</title>
+      <meta charSet="utf-8" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Compare size of screens" />
-      <link rel="icon" href="/images/favicon/favicon.ico" />
+      <meta name="description" content="Compare size of displays" />
+      <meta name="og:title" content="How big is your display" />
+      <meta name="og:description" content="Compare size of displays" />
+      <meta name="og:site_name" content="Howbigg" />
+      <link rel="icon" href="/favicon/favicon.ico" />
+
+      <title>{meta.title}</title>
+      <meta name="robots" content={meta.robots} />
+      <meta content={meta.description} name="description" />
+
+      {/* Open Graph */}
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.siteName} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+
+      {/* Twitter */}
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+
+      {/* Favicons */}
+      {favicons.map((linkProps) => (
+        <link key={linkProps.href} {...linkProps} />
+      ))}
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
     </>
   );
 }
+
+const favicons: Array<React.ComponentPropsWithoutRef<"link">> = [
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon/favicon-16x16.png",
+  },
+  { rel: "manifest", href: "/favicon/site.webmanifest" },
+  {
+    rel: "mask-icon",
+    href: "/favicon/safari-pinned-tab.svg",
+    color: "#00e887",
+  },
+  { rel: "shortcut icon", href: "/favicon/favicon.ico" },
+];
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,9 +5,8 @@ export default function Header() {
     <header className="top-0 z-50 bg-white mb-5 shadow">
       <div className="layout h-16 flex items-center justify-between flex-wrap md:flex-nowrap">
         <a href="/" className="text-primary-500 font-bold flex items-center">
-          {/*<img src="/images/favicon/favicon-32x32.png" alt="abc" />*/}
           <Image
-            src="/images/logo.png"
+            src="/favicon/android-chrome-512x512.png"
             alt="Logo of the application"
             width={32}
             height={32}
