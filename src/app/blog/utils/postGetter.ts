2b0c8651d342c4a35b5bf10a1ff688c014c5df4a
Jacek Obst <jacekobst1@gmail.com>
2023-04-12 19:31:15 +0200

    Sort posts - newest on top

--- src/app/blog/utils/postGetter.ts
+++ src/app/blog/utils/postGetter.ts
@@ -4,19 +4,21 @@ import { PostData } from "@/app/blog/types/PostData";
 
 const getPosts = (): PostData[] => {
   const files = fs.readdirSync("posts/");
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
-  return markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
-    const matterResult = matter(fileContents);
+  return files
+    .filter((file) => file.endsWith(".md"))
+    .map((fileName) => {
+      const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
+      const matterResult = matter(fileContents);
 
-    return {
-      date: matterResult.data.date,
-      title: matterResult.data.title,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
+      return {
+        date: matterResult.data.date,
+        title: matterResult.data.title,
+        subtitle: matterResult.data.subtitle,
+        slug: fileName.replace(".md", ""),
+      };
+    })
+    .sort((a, b) => (a.date < b.date ? 1 : -1));
 };
 
 export default getPosts;
