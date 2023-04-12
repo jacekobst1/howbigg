7672ddee3aef9ace6f8591271db824df8cdae7fd
Jacek Obst <jacekobst1@gmail.com>
2023-04-12 19:42:02 +0200

    Add return button to post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Markdown from "markdown-to-jsx";
 import { getPostBySlug, getPosts } from "@/app/blog/utils/postGetter";
+import ArrowLink from "@/components/links/ArrowLink";
 
 interface PostProps {
   params: {
@@ -19,6 +20,9 @@ export default function PostPage({ params: { slug } }: PostProps) {
   const post = getPostBySlug(slug);
   return (
     <div>
+      <ArrowLink href="/blog" direction="left">
+        Back to blog
+      </ArrowLink>
       <div className="my-12 text-center">
         <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
         <p className="text-slate-400 mt-2">{post.data.date}</p>
