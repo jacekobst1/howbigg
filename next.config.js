4bb40f8e793ccae515f92f7c27a8e4122dd6ded2
Jacek Obst <jacekobst1@gmail.com>
2023-05-15 18:11:11 +0200

    feat: redirect www to non-www pages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/compare/display",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.howbigg.com" }],
+        destination: "https://howbigg.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
