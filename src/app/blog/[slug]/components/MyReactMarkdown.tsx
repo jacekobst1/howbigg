c3cfbf3b97c1492f1c71743f262c3edd27f33989
Jacek Obst <jacekobst1@gmail.com>
2023-05-15 21:33:49 +0200

    WiP: optimize image loading

--- src/app/blog/[slug]/components/MyReactMarkdown.tsx
+++ src/app/blog/[slug]/components/MyReactMarkdown.tsx
@@ -5,12 +5,17 @@ import { generateIdFromText } from "@/app/blog/[slug]/utils/anchorGenerator";
 import ReactMarkdown from "react-markdown";
 import { Post } from "@/app/blog/types/Post";
 import UnderlineLink from "@/components/links/UnderlineLink";
+import Image from "next/image";
 
 interface MyReactMarkdownProps {
   post: Post;
+  imageSizes: Record<string, { width: number; height: number }>;
 }
 
-export default function MyReactMarkdown({ post }: MyReactMarkdownProps) {
+export default function MyReactMarkdown({
+  post,
+  imageSizes,
+}: MyReactMarkdownProps) {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
@@ -29,6 +34,21 @@ export default function MyReactMarkdown({ post }: MyReactMarkdownProps) {
         a: ({ children, ...props }) => (
           <UnderlineLink href={props.href ?? ""}>{children}</UnderlineLink>
         ),
+        img: ({ src, alt, ...props }) => {
+          if (imageSizes[src as string]) {
+            const { width, height } = imageSizes[src as string];
+            return (
+              <Image
+                src={src as string}
+                alt={alt as string}
+                width={width}
+                height={height}
+              />
+            );
+          } else {
+            return <img src={src} alt={alt} />;
+          }
+        },
       }}
     >
       {post.content}
