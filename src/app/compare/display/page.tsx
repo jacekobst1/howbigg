e39043b3d88390a52d793f550ccf226efa36acf5
Jacek Obst <jacekobst1@gmail.com>
2023-06-12 21:47:58 +0200

    Add dividers on main page

--- src/app/compare/display/page.tsx
+++ src/app/compare/display/page.tsx
@@ -6,11 +6,11 @@ import BlogColumn from "@/components/shared/BlogColumn";
 export default function CompareDisplayPage() {
   return (
     <div className="compare-layout__container layout-xl">
-      <section className="compare-layout__section compare-layout__left-section" />
-      <section className="compare-layout__section compare-layout__center-section">
+      <section className="compare-layout__left-section" />
+      <section className="compare-layout__center-section">
         <Comparison />
       </section>
-      <section className="compare-layout__section compare-layout__right-section">
+      <section className="compare-layout__right-section">
         <BlogColumn />
       </section>
     </div>
