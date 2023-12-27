53a82fb32dd9f19acf322695ef954fdb38cecda6
Jacek Obst <jacek.obst@merito.pl>
2023-12-27 15:18:06 +0100

    Add contact mail to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,10 +5,16 @@ export default function Footer() {
 
   return (
     <footer className="w-full text-slate-500 py-4 mt-10 border-t border-slate">
-      <div className="footer footer-center items-center select-none">
-        <p>
-          © {currentYear} {config.shortUrl} - All rights reserved
-        </p>
+      <div className="footer footer-center items-center">
+        <div className="md:flex">
+          <p>
+            © {currentYear} {config.shortUrl} - All rights reserved
+          </p>
+          <p className="px-1 hidden md:block">|</p>
+          <p>
+            Contact: <a href="mailto:jacek@howbigg.com" className="text-primary-500">jacek@howbigg.com</a>
+          </p>
+        </div>
       </div>
     </footer>
   );
