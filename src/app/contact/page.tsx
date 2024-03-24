5ef43da9bf7b9a8194bb4883cfb4b98e9b29e571
Jacek Obst <jacek.obst@merito.pl>
2024-03-24 11:39:32 +0100

    Add more text to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,10 +17,25 @@ export const metadata: Metadata = {
 export default function ContactPage() {
   return (
     <div className="layout-md text-center">
-      <h3 className="mt-20">
-        Hi! Contact me by e-mail at
+      <h3 className="mt-10 lg:mt-20">
+        Hi, if you would like to contact me, please send an email to
         <a href="mailto:jacek@howbigg.com" className="text-primary-500 ml-1">jacek@howbigg.com</a>
       </h3>
+
+      <div className="pt-10">
+        <span>
+        I would be happy to hear comments or requests about the operation of the site.
+          I am also open to cooperation or marketing activities.
+          Here you can also make requests regarding the processing of your personal data (see <a
+          href="/privacy-policy.html" target="_blank" className="text-primary-500">Privacy policy</a> for more information).
+        </span>
+      </div>
+
+      <div className="pt-5">
+        <span>
+          I kindly ask you <b>not to send proposals related to improving SEO</b>, as I am not interested.
+        </span>
+      </div>
     </div>
   );
 }
