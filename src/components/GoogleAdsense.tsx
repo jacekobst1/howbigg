bf75be027ecc1309199d7eb65705919c6f9de46a
Jacek Obst <jacek.obst@merito.pl>
2024-03-24 07:23:34 +0100

    Chang GoogleAdsense load strategy to lazyOnLoad

--- src/components/GoogleAdsense.tsx
+++ src/components/GoogleAdsense.tsx
@@ -5,8 +5,8 @@ import Script from "next/script";
 
 export default function GoogleAdsense() {
   return <Script
-    id="adsbygoogle-init"
-    strategy="afterInteractive"
+    async
+    strategy="lazyOnload"
     crossOrigin="anonymous"
     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9372955055190705"
   />
